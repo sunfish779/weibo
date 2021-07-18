6a2ebea7cadd7dab51f0148a62869c941c72cb07
Kenny <sunfish779@gmial.com>
2021-07-18 03:01:32 +0000

    error

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,26 +8,28 @@
                 <h5>注册</h5>
             </div>
             <div class="card-body">
+                @include('shared._errors')
+
                 <form method="POST" action="{{ route('users.store') }}">
                     {{ csrf_field() }}
                     <div class="mb-3">
                         <label for="name" class="form-label">名称：</label>
-                        <input type="text" class="form-control" id="name" value="{{ old('name') }}">
+                        <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="email" class="form-label">邮箱：</label>
-                        <input type="email" class="form-control" id="email" value="{{ old('email') }}">
+                        <input type="email" class="form-control" name="email"  id="email" value="{{ old('email') }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="password" class="form-label">密码：</label>
-                        <input type="password" class="form-control" id="password" value="{{ old('password') }}">
+                        <input type="password" class="form-control" name="password" id="password" value="{{ old('password') }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="password_confirmation" class="form-label">确认密码：</label>
-                        <input type="password" class="form-control" id="password_confirmation"
+                        <input type="password" class="form-control" name="password_confirmation" id="password_confirmation"
                             value="{{ old('password_confirmation') }}">
                     </div>
 
