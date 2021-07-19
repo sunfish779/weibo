9c9ecd588a4636e24556e259ee6f0806baf8fb2a
Kenny <sunfish779@gmial.com>
2021-07-19 17:06:56 +0000

    create session

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,14 +3,14 @@
 
 @section('content')
 <div class="row">
-
-    <div class="col-md-2  mx-auto">
-
+  <div class="offset-md-2 col-md-8">
+    <div class="col-md-12">
+      <div class="offset-md-2 col-md-8">
         <section class="user_info">
           @include('shared._user_info', ['user' => $user])
         </section>
-
+      </div>
     </div>
-
+  </div>
 </div>
 @stop
