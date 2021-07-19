cfc3a2a9945aec36a45b94969ab1ef3faa61ca04
Kenny <sunfish779@gmial.com>
2021-07-19 17:42:00 +0000

    登录时记住我

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container ">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-2">
+  <div class="container">
     <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
       @if (Auth::check())
