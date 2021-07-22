22855792e93710557c90072a3661a19e087ebbcb
Kenny <sunfish779@gmial.com>
2021-07-22 20:44:24 +0000

    移除邮件发送的 from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -104,13 +104,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'summer@example.com';
-        $name = 'Summer';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
         Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+            $message->to($to)->subject($subject);
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,13 @@ Route::resource('users','UsersController');
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
-Route::delete('login', 'SessionsController@destroy')->name('logout');
+Route::delete('loginout', 'SessionsController@destroy')->name('logout');
 
 Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
 
 Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
 
